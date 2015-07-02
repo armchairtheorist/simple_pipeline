caa41658420424a283b4a928275213920c01d9dc
Jonathan Wong <jonathan@katsunews.com>
2015-07-02 15:18:13 +0800

    refactoring

--- lib/simple_pipeline.rb
+++ lib/simple_pipeline.rb
@@ -1,2 +1,36 @@
-require "simple_pipeline/pipe"
 require "simple_pipeline/version"
+
+
+class SimplePipeline
+
+
+	def initialize
+		@pipes = []
+	end
+
+
+	def add (pipe)
+		begin
+			raise ArgumentError, "invalid pipe - incorrect number of arguments for process() method (should be 1)" unless pipe.class.instance_method(:process).arity == 1
+		rescue NameError
+			raise ArgumentError, "invalid pipe - process() method not found"
+		end
+
+		@pipes << pipe
+	end
+
+
+	def size
+		return @pipes.size
+	end
+
+
+	def process (payload)
+		@pipes.each do |pipe|
+			pipe.process(payload)
+		end
+	end
+
+
+	alias :length :size
+end
--- lib/simple_pipeline/version.rb
+++ lib/simple_pipeline/version.rb
@@ -1,3 +1,3 @@
-module SimplePipeline
+class SimplePipeline
   VERSION = "0.0.1"
 end
