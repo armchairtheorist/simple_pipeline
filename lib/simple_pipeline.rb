d83b779f97eac54f1724a3f56e7b5da730cab9e1
Jonathan Wong <jonathan@katsunews.com>
2016-07-03 19:18:43 +0800

    tabs to spaces

--- lib/simple_pipeline.rb
+++ lib/simple_pipeline.rb
@@ -3,35 +3,33 @@ require "simple_pipeline/version"
 
 
 class SimplePipeline
+    def initialize
+        @pipes = []
+    end
 
-	def initialize
-		@pipes = []
-	end
+    def add (pipe)
+        begin
+            raise ArgumentError, "invalid pipe - incorrect number of arguments for process() method (should be 1)" unless pipe.class.instance_method(:process).arity == 1
+        rescue NameError
+            raise ArgumentError, "invalid pipe - process() method not found"
+        end
 
-	def add (pipe)
-		begin
-			raise ArgumentError, "invalid pipe - incorrect number of arguments for process() method (should be 1)" unless pipe.class.instance_method(:process).arity == 1
-		rescue NameError
-			raise ArgumentError, "invalid pipe - process() method not found"
-		end
+        @pipes << pipe
+    end
 
-		@pipes << pipe
-	end
+    def size
+        return @pipes.size
+    end
 
-	def size
-		return @pipes.size
-	end
+    def process (payload)
+        @pipes.each do |pipe|
+            if pipe.is_a? SimplePipeline::Timeout
+                pipe.process_with_timeout(payload)
+            else
+                pipe.process(payload)
+            end
+        end
+    end
 
-	def process (payload)
-		@pipes.each do |pipe|
-			if pipe.is_a? SimplePipeline::Timeout
-				pipe.process_with_timeout(payload)
-			else
-				pipe.process(payload)
-			end
-		end
-	end
-
-	alias :length :size
-
+    alias :length :size
 end
--- lib/simple_pipeline/version.rb
+++ lib/simple_pipeline/version.rb
@@ -1,3 +1,3 @@
 class SimplePipeline
-  VERSION = "0.0.2"
+    VERSION = "0.0.2"
 end
