c13fcf72044eef6dff39cf5c264a5151943a9dca
Jonathan Wong <jonathan@katsunews.com>
2016-07-03 19:15:56 +0800

    added class-level timeout values and changed indentation to spaces

--- lib/simple_pipeline/timeout.rb
+++ lib/simple_pipeline/timeout.rb
@@ -1,19 +1,30 @@
 require 'timeout'
 
 class SimplePipeline
+    module Timeout
+        def self.included (base)
+            base.extend(ClassMethods)
+            base.class_variable_set(:@@timeout_in_sec, 0)
+        end
 
-	module Timeout
+        module ClassMethods
+            def set_timeout (sec)
+                self.class_variable_set(:@@timeout_in_sec, sec)
+            end
+        end
 
-		def set_timeout (sec)
-			@timeout_in_sec = sec
-		end
+        def timeout=(sec)
+            @timeout_in_sec = sec
+        end
 
-		def process_with_timeout (payload)
-			::Timeout::timeout(@timeout_in_sec) {
-				process(payload)
-			}
-		end
+        def process_with_timeout (payload)
+            ::Timeout::timeout(timeout) {
+                process(payload)
+            }
+        end
 
-	end
-
+        def timeout
+            @timeout_in_sec || self.class.class_variable_get(:@@timeout_in_sec)
+        end
+    end
 end
--- spec/simple_pipeline_spec.rb
+++ spec/simple_pipeline_spec.rb
@@ -1,63 +1,65 @@
 require 'spec_helper'
 
 class TestPipe
-	def process (payload)
-		payload[:test_value] *= 10
-	end
+    def process (payload)
+        payload[:test_value] *= 10
+    end
 end
 
 class BadPipe
-	def process (payload1, payload2)
-		# do nothing
-	end
+    def process (payload1, payload2)
+        # do nothing
+    end
 end
 
 class TimeoutPipe
-	include SimplePipeline::Timeout
+    include SimplePipeline::Timeout
 
-	def initialize
-		set_timeout 1 # seconds
-	end
+    set_timeout 3 # seconds
 
-	def process (payload)
-		sleep 10 # seconds
-	end
+    def process (payload)
+        sleep 10 # seconds
+    end
 end
 
 describe SimplePipeline do
-	it "should support three normal pipes" do
-		pipeline = SimplePipeline.new
-		pipeline.add TestPipe.new
-		pipeline.add TestPipe.new
-		pipeline.add TestPipe.new
-		
-		payload = {:test_value => 10}
+    it "should support three normal pipes" do
+        pipeline = SimplePipeline.new
+        pipeline.add TestPipe.new
+        pipeline.add TestPipe.new
+        pipeline.add TestPipe.new
+        
+        payload = {:test_value => 10}
 
-		pipeline.process(payload)
+        pipeline.process(payload)
 
-		expect(pipeline.size).to eq 3
-    	expect(payload[:test_value]).to eq 10000
-  	end
+        expect(pipeline.size).to eq 3
+        expect(payload[:test_value]).to eq 10000
+    end
 
-  	it "should throw an error for invalid pipes" do
-		pipeline = SimplePipeline.new
-		
-		expect {
-			pipeline.add Object.new # not a pipe
-		}.to raise_error(ArgumentError)
+    it "should throw an error for invalid pipes" do
+        pipeline = SimplePipeline.new
+        
+        expect {
+            pipeline.add Object.new # not a pipe
+        }.to raise_error(ArgumentError)
 
-		expect {
-			pipeline.add BadPipe
-		}.to raise_error(ArgumentError)
-	end
+        expect {
+            pipeline.add BadPipe
+        }.to raise_error(ArgumentError)
+    end
 
-	it "should support pipes with timeout" do
-		pipeline = SimplePipeline.new
-		pipe = TimeoutPipe.new
-		pipeline.add pipe
+    it "should support pipes with timeout" do
+        pipeline = SimplePipeline.new
+        pipe = TimeoutPipe.new
+        pipeline.add pipe
 
-		expect {
-			pipeline.process({})
-		}.to raise_error(Timeout::Error)
-	end
+        expect(pipe.timeout).to eq 3
+        pipe.timeout = 1
+        expect(pipe.timeout).to eq 1
+
+        expect {
+            pipeline.process({})
+        }.to raise_error(Timeout::Error)
+    end
 end
